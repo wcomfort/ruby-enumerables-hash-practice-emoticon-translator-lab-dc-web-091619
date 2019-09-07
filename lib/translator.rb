b3f1ce56bbbdff698fedeaa4b5919b4c7c4a267d
Will Comfort <comfortwb21@gmail.com>
2019-09-07 19:01:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ end
 
 def get_japanese_emoticon(path, emoticon)
   emoticon_hash = load_library(path)
-  trans = 
+  trans = emoticon_hash["get_emoticon"][emoticon]
 end
 
 def get_english_meaning
