ef2f07c203919f2c481bb141af7c67567df1d7c5
Will Comfort <comfortwb21@gmail.com>
2019-09-07 19:02:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def get_japanese_emoticon(path, emoticon)
   emoticon_hash = load_library(path)
   trans = emoticon_hash["get_emoticon"][emoticon]
   if trans == nil 
-    trans = "Sorry"
+    trans = "Sorry, that emoticon was not found"
   end
   trans 
 end
