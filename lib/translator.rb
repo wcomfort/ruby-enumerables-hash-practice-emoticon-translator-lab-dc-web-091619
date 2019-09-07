9486412b89d134abe11da69760f81bcf0b8c1789
Will Comfort <comfortwb21@gmail.com>
2019-09-07 18:50:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,12 @@
 require "yaml"
+require "pry"
 def load_library(path)
 emot_trans = YAML.load_file(path)
 emoticon_hash = {
   "get_meaning" => {},
   "get_emoticon" => {}
 }
-
+emot_trans.each do |
 end
 
 def get_japanese_emoticon
