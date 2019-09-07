c69238a95b3554ac5c098f9a9dbe9bf0bdf5cbd5
Will Comfort <comfortwb21@gmail.com>
2019-09-07 18:58:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,9 +3,7 @@ require "pry"
 def load_library(path)
 emot_trans = YAML.load_file(path)
 
-emoticon_hash = {
-  "get_meaning" => {},
-  "get_emoticon" => {}
+emoticon_hash = {"get_meaning" => {}, "get_emoticon" => {}
 }
 emot_trans.each do |english_word, emoticon_set|
     emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
