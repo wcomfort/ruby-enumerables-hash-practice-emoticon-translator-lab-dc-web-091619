bd5033d516b8f68fafbcfac74e152f2c2282cb2d
Will Comfort <comfortwb21@gmail.com>
2019-09-07 18:59:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,15 +5,15 @@ emot_trans = YAML.load_file(path)
 
 emoticon_hash = {"get_meaning" => {}, "get_emoticon" => {}}
 
-emot_trans.each do |english_word, emoticon_set|
-    emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
-    emoticon_hash["get_meaning"][emoticon_set.last] = english_word
+emot_trans.each do |key, value|
+    emoticon_hash["get_emoticon"][value.first] = value.last
+    emoticon_hash["get_meaning"][value.last] = key
   end
   emoticon_hash
 end
 
 def get_japanese_emoticon
-  # code goes here
+  
 end
 
 def get_english_meaning
